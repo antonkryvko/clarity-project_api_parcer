7011377f5e9d9d6ec4cb88e93192a1a677416a08
daryaliha <daryaliha@gmail.com>
2020-10-04 20:16:12 +0300

    Parser update (#1)
    * added directors, status, contacts, main kved fields to a parser
    
    * changed columns added to dataframe
    
    * made requirement.txt with pip freeze
    
    * fixed bugs
    
    * deleted prints
    
    * test scrapper output

--- settings.py
+++ settings.py
@@ -18,6 +18,11 @@ CAPITAL_COLUMN_HEADER = 'Статутний капітал'
 BENEFICIARY_COLUMN_HEADER = 'Бенефіціар'
 FOUNDER_COLUMN_HEADER = 'Засновник'
 VAT_COLUMN_HEADER = 'Платник податків'
+MAIN_KVED_COLUMN_HEADER = 'Основний КВЕД'
+STATUS_COLUMN_HEADER = 'Стан'
+CONTACTS_COLUMN_HEADER = 'Контакти'
+DIRECTORS_COLUMN_HEADER = 'Уповноважені особи'
+
 
 EDR_DATA_JSON_KEY = 'edr_data'
 REGISTRATION_JSON_KEY = 'registration'
@@ -29,3 +34,9 @@ SHARE_CAPITAL_JSON_KEY = 'capital'
 VAT_JSON_KEY = 'vat'
 
 REGISTRATION_REGEXP = '^\n\d{2}\.\d{2}\.\d{4}\n$'
+FOUNDERS_CSS_SELECTOR = 'div.founder > div.name > a'
+CAPITAL_REGEXP = "Статутний капітал: \D+([\d .])+"
+KVED_TD = "Види діяльності: " 
+DIRECTORS_TD = "Уповноважені особи:"
+STATUS_CSS_SELECTOR = "td.edr-status"
+CONTACTS_REGEXP = "tel:(\d)+"
